3c3a51f9dfa4793c5b91dfe584ea713377cb2d26
gurlal57 <48442076+gurlal57@users.noreply.github.com>
2019-03-24 14:12:53 -0400

    Update main2.c

--- main2.c
+++ main2.c
@@ -1,8 +1,12 @@
 /*PROGRAM SOURCE - https://www.github.com/gs369/mushroompi
 /*LIBRARY*/
 #include<stdio.h>
+#include <dirent.h>
 #include<wiringPi.h>
+#include <fcntl.h>
 #include<string.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include<wiringSerial.h>
 /*DECLARAION OF RASPBERRYPI PINS*/
 #define RELAY 9 	//SOIL
@@ -25,6 +29,7 @@ char *dash(char *buf, int len ) {
 int main ()
 {
 	char buf[60];
+	float x,tempr;
 	printf("%s\n", dash(buf,60));
 	printf("PROJECT     : EMBEDDING EMBEDDED IN MUSHROOM GROWING\n");
 	printf("%s\n", dash(buf,60));
@@ -40,7 +45,7 @@ int main ()
 	int fd, a; //PREDEFINED INT
 	char *b = "t" , *d = "m" , *e = "l" , *f = "i" , *g = "p" ;
 	char *txt1 = "z" , *txt2 = "y" , *txt3 = "x" , *txt4 = "w" , *txt5 = "v" ;
-	char *stat1 = "u" , *stat2 = "s" , *stat3 = "r" , *stat4 = "q" , *stat5 = "n" , *stat6 = "o" ;
+	char *stat1 = "u" , *stat2 = "s" , *stat3 = "r" , *stat4 = "q" , *stat5 = "n" , *stat6 = "o" ,*stat7 = "k" ;
 	char *gsm1 = "e" , *gsm2 = "f" , *gsm3 = "g" ;
 	fd = serialOpen("/dev/ttyAMA0",115200); //UART at 115200, ttyAMA0.
 	/*GPIO*/
@@ -93,18 +98,25 @@ int main ()
 		//temp reading*/
 		 if((a) == *gsm3){
 	//call temp reading fun here*/
-                   if (tempc > 22){ 
+			 tempr = tempread(x);
+                   if (tempr > 22){ 
 	delay(100);
 	printf("high temp");
 	fflush(stdout);
 	serialPutchar(fd, *stat1);
 	}
-	else if(tempc < 15){
+	else if(tempr < 15){
 	delay(100);
 	printf("low temp");
 	fflush(stdout);
 	serialPutchar(fd, *stat2);
-	}}
+	}
+	else if(15 < tempr < 22){
+	delay(100);
+	printf("normal temp");
+	fflush(stdout);
+	serialPutchar(fd, *stat7);		 
+		 }
 		//driving output relays*/
 	if((a) == *b){
 	digitalWrite(RELAY,HIGH);
@@ -155,5 +167,52 @@ int main ()
 	}
 }
 }
+//function to read temperature from ds18b20*/
 
+float tempread(float x)
+{ float tempc;
+DIR *dir;
+ struct dirent *dirent;
+ char dev[16];      // Dev ID
+ char devPath[128]; // Path to device
+ char buf[256];     // Data from device
+ char tmpData[6];   // Temp C * 1000 reported by device 
+ char path[] = "/sys/bus/w1/devices"; 
+ ssize_t numRead;
+ 
+ dir = opendir (path);
+ if (dir != NULL)
+ {
+  while ((dirent = readdir (dir)))
+   // 1-wire devices are links beginning with 28-
+   if (dirent->d_type == DT_LNK && 
+     strstr(dirent->d_name, "28-") != NULL) { 
+    strcpy(dev, dirent->d_name);
+    printf("\nDevice: %s\n", dev);
+   }
+        (void) closedir (dir);
+        }
+ else
+ {
+  perror ("Couldn't open the w1 devices directory");
+  return 1;
+ }
+
+        // Assemble path to OneWire device
+ sprintf(devPath, "%s/%s/w1_slave", path, dev);
+ int fd = open(devPath, O_RDONLY);
+  if(fd == -1)
+  {
+   perror ("Couldn't open the w1 device.");
+   return 1;   
+  }
+  while((numRead = read(fd, buf, 256)) > 0) 
+  {
+   strncpy(tmpData, strstr(buf, "t=") + 2, 5); 
+   float tempC = strtof(tmpData, NULL);
+   
+  }
+  close(fd);
+  return(tempc/1000);
+  }
 	
