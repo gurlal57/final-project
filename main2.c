4888f0f55671fb0a0f61454cc0748bd78ac2b2c5
gurlal57 <48442076+gurlal57@users.noreply.github.com>
2019-03-26 09:44:24 -0400

    Update main2.c

--- main2.c
+++ main2.c
@@ -8,6 +8,55 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include<wiringSerial.h>
+//function to read temperature from ds18b20*/
+
+float tempread(float x)
+{ float tempc;
+DIR *dir;
+ struct dirent *dirent;
+ char dev[16];      // Dev ID
+ char devPath[128]; // Path to device
+ char buf[256];     // Data from device
+ char tmpData[6];   // Temp C * 1000 reported by device 
+ char path[] = "/sys/bus/w1/devices"; 
+ ssize_t numRead;
+ 
+ dir = opendir (path);
+ if (dir != NULL)
+ {
+  while ((dirent = readdir (dir)))
+   // 1-wire devices are links beginning with 28-
+   if (dirent->d_type == DT_LNK && 
+     strstr(dirent->d_name, "28-") != NULL) { 
+    strcpy(dev, dirent->d_name);
+    printf("\nDevice: %s\n", dev);
+   }
+        (void) closedir (dir);
+        }
+ else
+ {
+  perror ("Couldn't open the w1 devices directory");
+  return 1;
+ }
+
+        // Assemble path to OneWire device
+ sprintf(devPath, "%s/%s/w1_slave", path, dev);
+ int fd = open(devPath, O_RDONLY);
+  if(fd == -1)
+  {
+   perror ("Couldn't open the w1 device.");
+   return 1;   
+  }
+  while((numRead = read(fd, buf, 256)) > 0) 
+  {
+   strncpy(tmpData, strstr(buf, "t=") + 2, 5); 
+   float tempC = strtof(tmpData, NULL);
+   
+  }
+  close(fd);
+  return(tempc/1000);
+  }
+	
 /*DECLARAION OF RASPBERRYPI PINS*/
 #define RELAY 9 	//SOIL
 #define RELAY2 11 	//TEMPERATURE
@@ -172,52 +221,3 @@ int main ()
 	}
 }
 }
-//function to read temperature from ds18b20*/
-
-float tempread(float x)
-{ float tempc;
-DIR *dir;
- struct dirent *dirent;
- char dev[16];      // Dev ID
- char devPath[128]; // Path to device
- char buf[256];     // Data from device
- char tmpData[6];   // Temp C * 1000 reported by device 
- char path[] = "/sys/bus/w1/devices"; 
- ssize_t numRead;
- 
- dir = opendir (path);
- if (dir != NULL)
- {
-  while ((dirent = readdir (dir)))
-   // 1-wire devices are links beginning with 28-
-   if (dirent->d_type == DT_LNK && 
-     strstr(dirent->d_name, "28-") != NULL) { 
-    strcpy(dev, dirent->d_name);
-    printf("\nDevice: %s\n", dev);
-   }
-        (void) closedir (dir);
-        }
- else
- {
-  perror ("Couldn't open the w1 devices directory");
-  return 1;
- }
-
-        // Assemble path to OneWire device
- sprintf(devPath, "%s/%s/w1_slave", path, dev);
- int fd = open(devPath, O_RDONLY);
-  if(fd == -1)
-  {
-   perror ("Couldn't open the w1 device.");
-   return 1;   
-  }
-  while((numRead = read(fd, buf, 256)) > 0) 
-  {
-   strncpy(tmpData, strstr(buf, "t=") + 2, 5); 
-   float tempC = strtof(tmpData, NULL);
-   
-  }
-  close(fd);
-  return(tempc/1000);
-  }
-	
