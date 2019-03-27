cfdaf21cd380f4bc2ffea3ef341b11b8dc28af77
gurlal57 <48442076+gurlal57@users.noreply.github.com>
2019-03-27 12:37:46 -0400

    Update pi3serial.c

--- pi3serial.c
+++ pi3serial.c
@@ -2,21 +2,24 @@
 #include<wiringPi.h>
 #include<wiringSerial.h>
 int main(){
-	int fd;
-	fd = serialOpen("/dev/ttyAMA0",115200);
-	char c,b[10];
-	char *a = "t";
-	wiringPiSetupGpio();
+        int fd;
+        fd = serialOpen("/dev/ttyAMA0",115200);
+        char c,b[10];
+char *stat = b;
+        char *a = "t";
+char *f = "z";
+        wiringPiSetupGpio();
 while(1)
-	{
-	//putchar(serialGetchar(fd));
-	//fflush(stdout);
-	c = serialGetchar(fd);
-	printf("%c",c);
-	if((c) == *a){
-  scanf("%c",b)
-  fflush (stdout) ;
-	SerialPutchar(int fd,b)
-	}
+        {
+        //putchar(serialGetchar(fd));
+        //fflush(stdout);
+        c = serialGetchar(fd);
+        printf("%c",c);
+        if((c) == *a){
+    //printf("enter value of b\n");
+      //scanf("%c",b);
+  fflush(stdout);
+        serialPrintf(fd,f);
+        }
 }
 }
